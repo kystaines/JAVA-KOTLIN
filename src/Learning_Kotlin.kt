5f91aed696dc80ec8704efd4d9220f7810e52d83
Keziah Ysabel Sta. Ines <144651777+kystaines@users.noreply.github.com>
2024-12-29 00:04:30 +0800

    Update Learning_Kotlin.kt

--- src/Learning_Kotlin.kt
+++ src/Learning_Kotlin.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
     //This is a comment.
-    var name: String = "Keziah Ysabel Sta. Ines"+ " | " +
+    var name: String = "Keziah Ysabel Sta. Ines"//This is my Full Name. + " | " +
                         "Bachelor of Science in Information Systems" + " | " +
-                        "Technological University of the Philippines" //This is my Full Name
+                        "Technological University of the Philippines" //This is my Univeristy.
     val birthday: String = "2002-01-01"
     val age: Int = 23 //This is the Year I was Born.
     println(name)
